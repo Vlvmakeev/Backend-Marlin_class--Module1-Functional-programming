24c29540067a93a8b17e4fda4cf8434c167fbe21
Vlvmakeev <vlvmakeev88@gmail.com>
2022-04-03 13:55:56 +0700

    Add RefPart1_2
    Create file "functions.php, redirect functions from register.php to functions.php", refactoring for register.php, page_register.php, page_login.php

---
+++ RefPart1_2/functions.php
@@ -0,0 +1,31 @@
+<?php
+    function get_user_by_email($email){
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=part1', 'root', '');
+        $sql = "SELECT * FROM users WHERE email='$email'";
+        $statement = $pdo->query($sql);
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+        return $user['email'];}
+
+    function add_user($email, $password){
+        $pdo = new PDO('mysql:host=127.0.0.1;dbname=part1', 'root', '');
+        $sql = "INSERT INTO users (email, password) VALUES ('$email', '$password')";
+        $statement = $pdo->prepare($sql);
+        $statement->execute();
+        $sql = "SELECT * FROM users WHERE email='$email'";
+        $statement = $pdo->query($sql);
+        $user = $statement->fetch(PDO::FETCH_ASSOC);
+
+        return $user['id'];}
+
+    function set_flash_message($name, $message){
+        $_SESSION[$name] = $message;}
+
+    function display_flash_message($name){
+        if(isset($_SESSION[$name])){
+            echo "<div class=\"alert alert-{$name} text-dark\" role=\"alert\">{$_SESSION[$name]}</div>";
+            unset($_SESSION[$name]);}}
+
+    function redirect_to($path){
+        header("Location: $path");}
+?>
