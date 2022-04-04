a95bf61d889bad7ae80d9a39c7c8b34dfdc082e7
Vlvmakeev <vlvmakeev88@gmail.com>
2022-04-04 09:12:17 +0700

    Add Part 2
    Added Part 2, where I realized functions "verify" and "authorization", create script "auth.php".

---
+++ Part2/register.php
@@ -0,0 +1,21 @@
+<?php
+    session_start();
+    require 'functions.php';
+    
+    $email = $_POST['emailverify'];
+    $password = password_hash($_POST['userpassword'], PASSWORD_DEFAULT);
+
+    $user = get_user_by_email($email);
+
+    if( !empty($user) ){
+        set_flash_message('danger', '<strong>Уведомление!</strong> Этот эл. адрес уже занят другим пользователем.');
+        redirect_to('page_register.php');
+        exit;
+    }else{
+        add_user($email, $password);
+        set_flash_message('success', 'Регистрация успешна');
+        redirect_to('page_login.php');
+        exit;
+    }
+    
+?>
